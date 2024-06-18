b5ae6146c3c54ac26e8bf24fe00dad48c2b157c3
Robert Houston <51903522+mastaginger@users.noreply.github.com>
2024-06-18 00:05:42 -0600

    Update backend.py
    clarify, bug fix. comments.

--- App/backend.py
+++ App/backend.py
@@ -1,17 +1,19 @@
+# This file defines the API and how it interacts, it also handles basic authentication, although i have not gotten that going completely as of yet. 
+#CRUD operations seem to work as of now. 
 
-#this file defines the endpoints for the API. CRUD operations, logins, etc. At the moment most of everything is a get request, but i plan on changing that. 
+#Defines the endpoints for the API. . At the moment most of everything is a get request, but i plan on changing that. 
 #based on examples from the FASTAPI website
-
+#search broken
 from fastapi import FastAPI, Depends, HTTPException, status
 from typing import Annotated
 from fastapi.staticfiles import StaticFiles
 from database import *
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel 
+from pydantic import BaseModel
 app = FastAPI()
 
-#need to make middleware more secure here. 
+#need to make middleware more secure here. As of now it accepts all connections. This endpoint would not be exposed to the internet so it may not be an issue. 
 origins = [
     "*",
 ]
@@ -23,8 +25,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-fake_users_db ={}
 
+fake_users_db = {}
 app = FastAPI()
 
 
@@ -68,7 +70,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         )
     return user
 
-#not used 
+
 async def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)]
 ):
@@ -99,18 +101,18 @@ async def read_users_me(
     return current_user
 
 
-@app.get("/")
-async def root():
-    return {"message": "world"}
-#Create
 
+#Create
+# authentication should be added here. maybe a "is employee" flag in database. 
 @app.get("/add/{Title}:{Author}:{Genre}:{Price}")
 async def addBook(Title,Author,Genre,Price):
     print("im creating a new one captain!S")
     return {"data":f' {addNewBook(Title,Author,Genre,Price)}'}
 
-#Reading can be un authorized. This will go to the fontend, and can be intrepreted to the frontend
+#Reading can be unauthorized. This will go to the fontend, and can be intrepreted to the frontend
 
+
+#searching does not work at the moment, but getting all books, or book by id does. 
 @app.get("/search/{author}")#returns only specified books by author or title
 async def getBookByAuthorOrTitle(author):
     return {"data":findBookByAuthorOrTitle(author)}
@@ -124,10 +126,10 @@ async def getAllBooks():
 
 
 
-#update-- should add 
+#update
 @app.get("/update/{id}:{newTitle}:{Author}:{Genre}:{Price}")#updates a book by given ID
 async def updateExistingBook(id,newTitle,Author,Genre,Price):
-    print("im updating an existing one")
+    print("im updating an existing one captain!")
     return {"data":f' {updateBook(id,newTitle,Author,Genre,Price)}'}
 
 
@@ -140,7 +142,7 @@ async def deleteBookById(id):
 app.mount("/static", StaticFiles(directory="static"), name="static")
 #change to post
 @app.get("/user/login/{username}:{password}")
-
+#sends hash receives hash? 
 async def login1(username,password):
     return {"data":loginUser(username,password)}
 
@@ -149,19 +151,16 @@ async def login1(username,password):
 @app.get("/users/search/all")
 async def getAllUsers():
     return {"data":getUsers()}
-    
 @app.get("/users/add/{Name}:{Address}:{Email}:{Password}")
 async def addUser(Name,Address,Email,Password):
     return {"data":addUser(Name,Address,Email,Password)}
-    
 @app.get("/users/update/{id}:{Name}:{Address}:{Email}")
 async def updateUser(id,Name,Address,Email):
     return {"data":updateUser(id,Name,Address,Email)}
-    
 #should be accepting a pass hash here if im being honest .. probably want the frontend to hash it? 
+#need to be authenticated before changes are allowed. 
 async def updateUserPass(id,Password):
     return {"data":updateUser(id,Password)}
-    
 @app.get("/user/delete/{id}")
 async def delUser(id):
     return {"data":delUser(id)}
